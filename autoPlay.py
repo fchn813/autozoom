18182e9dca546c04cd78eb1773c02d17d85fa5df
Tony Guo <fchn813@gmail.com>
2021-01-22 13:25:04 -0500

    working version

--- autoPlay.py
+++ autoPlay.py
@@ -30,33 +30,30 @@ def shareVideo() :
     pyautogui.click(shareSound)  
     time.sleep(1)
 
-    # print("start find optVideo icon")
-    # optVideo =pyautogui.locateCenterOnScreen(r'C:\autozoom\optVideo.png', confidence=confi)
-
     pyautogui.click(shareSound.x+200,shareSound.y)   
     time.sleep(1)
 
-    #start video play
+    #share video
     print("start find share icon")
     share =pyautogui.locateCenterOnScreen(r'C:\autozoom\share.png', confidence=confi)
     print(share)
     pyautogui.click(share)   
     time.sleep(1)
 
-    #show video panel
-    pyautogui.moveTo(800,0) 
-    time.sleep(2)
-    pyautogui.moveTo(1250,30)
-    time.sleep(2)
-    # pyautogui.moveTo(1300,260)
-    # time.sleep(1)
-    pyautogui.click(1300,260)
-
-
     #start video play
     x,y = pyautogui.size()
     pyautogui.click(x/2, y/2)
     pyautogui.hotkey('space')  #pause the video
+
+    #show video panel
+    pyautogui.moveTo(800,0,0.5) 
+    time.sleep(1)
+    pyautogui.moveTo(1250,30,0.5)
+    time.sleep(1)
+    pyautogui.moveTo(1300,260,0.5)
+    time.sleep(0.5)
+    pyautogui.click()
+
     
 #wake up screen
 shareVideo()
--- my_autozoom.py
+++ my_autozoom.py
@@ -14,10 +14,10 @@ def playVideoAndZoom():
 
     os.startfile(r'C:\Users\T420s\Desktop\HappyWalk\PlayHappyWalk.lnk')
     # os.startfile(r'C:\Users\T420s\De sktop\HappyWalk\happyWalkPlusSong.xspf')
-    time.sleep(0.5) 
+    time.sleep(1) 
     x,y = pyautogui.size()
     pyautogui.click(x/2, y/2)
-    time.sleep(0.5)
+    time.sleep(1)
     pyautogui.press('space')  #pause the video
 
     os.startfile(r'C:\Users\T420s\AppData\Roaming\Zoom\bin\Zoom.exe')
